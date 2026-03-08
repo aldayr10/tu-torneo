6a8e16f60251a1175c51e91ca9a8e3dca4d82ac8
Angie Lara <angielara@unimayor.edu.co>
2026-03-08 16:40:59 -0500

    Se creo la rama de join-team

---
+++ src/app/pages/join-team/join-team.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-team',
+  imports: [],
+  templateUrl: './join-team.html',
+  styleUrl: './join-team.css',
+})
+export class JoinTeam {}
