2c67b7af50b666c4492df4229868b47e30f3b818
Aldayr ramirez <j.ramirez@unimayor.edu.co>
2026-03-09 19:16:08 -0500

    integracion de la ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { Routes } from '@angular/router';
 import { RecoverPassword } from './pages/recover-password/recover-password';
 import { CheckEmail } from './pages/recover-password/check-email/check-email';
 import { Register } from './pages/register/register';
-import { UpdatePerfil } from './pages/update-perfil/update-perfil';
+
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: Login },
     { path: 'recover-password', component: RecoverPassword },
     { path: 'check-email', component: CheckEmail },
+    { path: 'register', component: Register },
 ];
 
