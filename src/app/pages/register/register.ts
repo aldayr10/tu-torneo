f20c32c0434e814e087fc723074fd289e02c5911
Dani Mendez <dfhernandez@unimayor.edu.co>
2026-03-09 19:50:15 -0500

    ,

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrl: './register.css'
 })
 export class Register {
+
+  jugador = {
+    nombre: '',
+    correo: '',
+    password: '',
+    confirmar: ''
+  };
+
+  registrar() {
+    console.log("Jugador registrado:", this.jugador);
+  }
 
 }
--- src/app/pages/update/actualizar.ts
+++ src/app/pages/update/actualizar.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-actualizar',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './actualizar.html',
-  styleUrl: './actualizar.css',
+  styleUrl: './actualizar.css'
 })
 export class Actualizar {
+
+  jugador = {
+    nombre: '',
+    correo: '',
+    password: ''
+  };
+
+  actualizarDatos() {
+    console.log("Datos actualizados:", this.jugador);
+  }
 
 }
