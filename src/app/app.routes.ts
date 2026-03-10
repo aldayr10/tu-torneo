f459f54202ff408403279fa71a089605723aa44e
Angie Lara <angielara@unimayor.edu.co>
2026-03-10 18:13:18 -0500

    join-team se saco del dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { CheckEmail } from './pages/recover-password/check-email/check-email';
 import { Register } from './pages/register/register';
 import { UpdateProfile } from './pages/update-profile/update-profile';
 import { Dashboard } from './pages/dashboard/dashboard';
-
+import { JoinTeam } from './pages/join-team/join-team';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -14,10 +14,8 @@ export const routes: Routes = [
     { path: 'recover-password', component: RecoverPassword },
     { path: 'check-email', component: CheckEmail },
     { path: 'register', component: Register },
-    { path: 'update-profile', component:UpdateProfile  },
-    { path: 'dashboard', component:Dashboard, 
-        children: [
-        ] 
-    },
+    { path: 'update-profile', component: UpdateProfile },
+    { path: 'dashboard', component: Dashboard},
+    { path: 'join-team', component: JoinTeam },
 ];
 
--- src/app/pages/dashboard/join-team/join-team.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-join-team',
-  imports: [],
-  templateUrl: './join-team.html',
-  styleUrl: './join-team.css',
-})
-export class JoinTeam {}
